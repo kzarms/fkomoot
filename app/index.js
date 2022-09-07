fe07eea8263759693e6a90336223b3b3c6262250
Konstantin <ikotpad@gmail.com>
2022-09-07 21:49:18 +0200

    Add touchable elements on the screen1, ready to modify screen 2

--- app/index.js
+++ app/index.js
@@ -1,31 +1,56 @@
 import * as document from 'document';
+import { me as appbit } from 'appbit';
 import { HeartRateSensor } from 'heart-rate';
 import * as messaging from 'messaging';
 
-let screenSelect = document.getElementById("screenSelect");
-let screenAction = document.getElementById("screenAction");
-//let screenResult = document.getElementById("result");
+// Define global var
+let EXCERSIZE = 0;
 
+// Define screens
+const screenSelect = document.getElementById('screenSelect');
+const screenAction = document.getElementById('screenAction');
+// Get item lists
+const list = document.getElementById('exerciseList');
+const items = list.getElementsByClassName('list-item');
+//
 const hrmText = document.getElementById('hrm');
 const updLable = document.getElementById('updated');
 const gpsText = document.getElementById('gps');
 const startBtn = document.getElementById('startBtn');
 
-hrmText.text = '--❤️';
-updLable.text = '...';
+// hrmText.text = '--❤️';
+// updLable.text = '...';
 
-const lastValueTimestamp = Date.now();
+// const lastValueTimestamp = Date.now();
 
-function showScreenSelect() {
-  console.log("Show screen select");
-  screenSelect.style.display = "inline";
-  screenAction.style.display = "none";
+// Functions
+function showScreenAction() {
+  console.log('Show screen action');
+  screenSelect.style.display = 'none';
+  screenAction.style.display = 'inline';
 }
 
-function showScreenAction() {
-  console.log("Show screen action");
-  screenSelect.style.display = "none";
-  screenAction.style.display = "inline";
+function showScreenSelect() {
+  console.log('Show screen select');
+  screenSelect.style.display = 'inline';
+  screenAction.style.display = 'none';
+  // Set listeners for items
+  items.forEach((element, index) => {
+    const touch = element.getElementById('touch');
+    if (touch !== null) {
+      touch.onclick = function (evt) {
+        // console.log(`touched: ${index}`);
+        if (index === 3) {
+          // Exit
+          appbit.exit();
+        } else {
+          // Set global var and switch to the second screen
+          EXCERSIZE = index;
+          showScreenAction();
+        }
+      };
+    }
+  });
 }
 
 function convertMsAgoToString(millisecondsAgo) {
@@ -41,7 +66,7 @@ function convertMsAgoToString(millisecondsAgo) {
 
 function updateDisplay() {
   if (lastValueTimestamp !== undefined) {
-    updLable.text = convertMsAgoToString(Date.now() - lastValueTimestamp);
+    // updLable.text = convertMsAgoToString(Date.now() - lastValueTimestamp);
   }
 }
 
@@ -54,36 +79,49 @@ function sendData(data) {
   }
 }
 
-if (HeartRateSensor) {
-  console.log('This device has a HeartRateSensor!');
-  const hrm = new HeartRateSensor();
-  hrm.addEventListener('reading', () => {
-    // Set color based on user profile
-    const hrRate = hrm.heartRate;
-    // Set HR value
-    if (hrRate === null) {
-      hrmText.text = '--❤️';
-    } else {
-      hrmText.text = `${hrRate}❤️`;
-      sendData(`{"hr":"${hrRate}"}`);
-    }
-  });
-  // Start
-  hrm.start();
-} else {
-  console.log('This device does NOT have a HeartRateSensor!');
+function main() {
+  // Show the main screen
+  showScreenSelect();
+
+  // // Track
+  // items.forEach((element, index) => {
+  //   let touch = element.getElementById("touch");
+  //   touch.onclick = function(evt) {
+  //     console.log(`touched: ${index}`);
+  //   };
+  // });
 }
 
-messaging.peerSocket.addEventListener('message', (evt) => {
-  console.log(evt.data.Latitude);
-  gpsText.text = evt.data.Latitude;
-});
+// if (HeartRateSensor) {
+//   console.log('This device has a HeartRateSensor!');
+//   const hrm = new HeartRateSensor();
+//   hrm.addEventListener('reading', () => {
+//     // Set color based on user profile
+//     const hrRate = hrm.heartRate;
+//     // Set HR value
+//     if (hrRate === null) {
+//       hrmText.text = '--❤️';
+//     } else {
+//       hrmText.text = `${hrRate}❤️`;
+//       sendData(`{"hr":"${hrRate}"}`);
+//     }
+//   });
+//   // Start
+//   hrm.start();
+// } else {
+//   console.log('This device does NOT have a HeartRateSensor!');
+// }
 
-showScreenSelect();
-startBtn.onclick = function (){
-  showScreenAction();
-}
-// Set infinity loop for update
-setInterval(updateDisplay, 1000);
+// messaging.peerSocket.addEventListener('message', (evt) => {
+//   console.log(evt.data.Latitude);
+//   gpsText.text = evt.data.Latitude;
+// });
 
-console.log('Exit');
+// showScreenSelect();
+// startBtn.onclick = function (){
+//   showScreenAction();
+// }
+// // Set infinity loop for update
+// setInterval(updateDisplay, 1000);
+
+main();
